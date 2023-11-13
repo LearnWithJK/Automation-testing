20fce2231e8eb551ef8dc9f1db20722a3ab08934
LearnWithJK <learn.automationwithjk@gmail.com>
2023-11-13 17:12:56 +0530

    modified

--- src/test/java/com/purna/runner/RunPurnaTest.java
+++ src/test/java/com/purna/runner/RunPurnaTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		tags = "@RegressionTest1", 
+		tags = "@RegressionTest", 
 		features = "src/test/resources/features", 
 		glue = "com.purna.stepdefinitions",
 		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
